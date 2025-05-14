baa1a6e9f5a9d5677667affbf5052da327ce2e67
Virle <geyingauv@gmail.com>
2025-05-14 23:48:26 +0800

    feat 用户相关修改

--- src/main/java/cool/auv/authspringbootstarter/controller/CurrentUserController.java
+++ src/main/java/cool/auv/authspringbootstarter/controller/CurrentUserController.java
@@ -2,9 +2,11 @@ package cool.auv.authspringbootstarter.controller;
 
 import cool.auv.authspringbootstarter.entity.SysRole;
 import cool.auv.authspringbootstarter.entity.SysUser;
+import cool.auv.authspringbootstarter.mapstruct.BaseSysRoleMapstruct;
 import cool.auv.authspringbootstarter.service.SysUserService;
 import cool.auv.authspringbootstarter.utils.SecurityContextUtil;
 import cool.auv.authspringbootstarter.vm.SysPermissionTreeVM;
+import cool.auv.authspringbootstarter.vm.SysRoleVM;
 import cool.auv.codegeneratorjpa.core.exception.AppException;
 import cool.auv.codegeneratorjpa.core.utils.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +14,10 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/current-user")
@@ -22,11 +26,18 @@ public class CurrentUserController {
     @Autowired
     private SysUserService sysUserService;
 
+    @Autowired
+    private BaseSysRoleMapstruct baseSysRoleMapstruct;
+
     @GetMapping("/get-role")
-    public ResponseEntity<Set<SysRole>> getCurrentRole() {
+    public ResponseEntity<Set<SysRoleVM>> getCurrentRole() {
         Optional<SysUser> currentUser = SecurityContextUtil.getCurrentUser();
         Optional<Set<SysRole>> sysRoles = currentUser.map(SysUser::getRoleSet);
-        return ResponseUtil.wrapOrNotFound(sysRoles);
+        Optional<Set<SysRoleVM>> sysRoleVMS = sysRoles.map(roleSet -> roleSet.stream().map(sysRole -> {
+            sysRole.setPermissionSet(new HashSet<>());
+            return baseSysRoleMapstruct.entityToVm(sysRole);
+        }).collect(Collectors.toSet()));
+        return ResponseUtil.wrapOrNotFound(sysRoleVMS);
     }
 
     @GetMapping("/permissions")
